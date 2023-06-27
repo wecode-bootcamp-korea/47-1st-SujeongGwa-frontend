9a1625204a50d2952e34111718746313faf74d01
최진이 <jini.choi@choejin-iui-MacBookAir.local>
2023-06-27 20:08:04 +0900

    ADD : Nav depth2 layout 구현

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,25 +5,65 @@ import './Nav.scss';
 const Nav = () => {
   return (
     <div className="nav scroll">
-      <div class="container">
+      <div class=" depth01">
         <ul className="mainMenu">
           <li className="logo">
             <Link to="/">
               <img src="./images/logo_fffef2.png" alt="logo" />
             </Link>
           </li>
-          {MENU_LIST_LEFT.map(info => (
-            <li className="menulist" key={info.id}>
-              <Link to={info.link}>{info.text}</Link>
-            </li>
-          ))}
+          <li className="menulist">
+            <Link to="/">PORCELAIN TILE</Link>
+          </li>
+          <li className="menulist">
+            <Link to="/">WALL TILE</Link>
+          </li>
+          <li className="menulist">
+            <Link to="/">FLOOR TILE</Link>
+          </li>
         </ul>
         <ul className="utilMenu">
-          {MENU_LIST_RIGHT.map(info => (
-            <li key={info.id}>
-              <Link to={info.link}>{info.text}</Link>
-            </li>
-          ))}
+          <li className="menulist">
+            <Link to="/">LOGIN</Link>
+          </li>
+          <li className="menulist">
+            <Link to="/">CART</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="depth02">
+        <ul className="porcelainTile on">
+          <li>
+            <Link to="/">600X1200X20MM</Link>
+          </li>
+          <li>
+            <Link to="/">600X1200X11MM</Link>
+          </li>
+          <li>
+            <Link to="/">600X600X20MM</Link>
+          </li>
+          <li>
+            <Link to="/">600X600X10MM</Link>
+          </li>
+          <li>
+            <Link to="/">400X800X11MM</Link>
+          </li>
+        </ul>
+        <ul className="wallTile">
+          <li>
+            <Link to="/">300X600MM</Link>
+          </li>
+          <li>
+            <Link to="/">200X600MM</Link>
+          </li>
+        </ul>
+        <ul className="floorTile">
+          <li>
+            <Link to="/">300X300MM</Link>
+          </li>
+          <li>
+            <Link to="/">200X400MM</Link>
+          </li>
         </ul>
       </div>
     </div>
@@ -32,13 +72,13 @@ const Nav = () => {
 
 export default Nav;
 
-const MENU_LIST_LEFT = [
-  { id: 1, link: '/', text: 'PORCELAIN TILE' },
-  { id: 2, link: '/', text: 'WALL TILE' },
-  { id: 3, link: '/', text: 'FLOOR TILE' },
-];
+// const MENU_LIST_LEFT = [
+//   { id: 1, link: '/', text: 'PORCELAIN TILE' },
+//   { id: 2, link: '/', text: 'WALL TILE' },
+//   { id: 3, link: '/', text: 'FLOOR TILE' },
+// ];
 
-const MENU_LIST_RIGHT = [
-  { id: 1, link: '/', text: 'LOGIN' },
-  { id: 2, link: '/', text: 'CART' },
-];
+// const MENU_LIST_RIGHT = [
+//   { id: 1, link: '/', text: 'LOGIN' },
+//   { id: 2, link: '/', text: 'CART' },
+// ];
