3cff6fac70da7a407bc9b20313e170b9b33ddaf2
최진이 <jini.choi@choejin-iui-MacBookAir.local>
2023-06-28 14:38:23 +0900

    ADD : main layout 초기세팅

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Main from './components/Main/Main';
+import Main from './pages/Main/Main';
 
 const Router = () => {
   return (
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,7 +2,25 @@ import React from 'react';
 import './Main.scss';
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <main className="Main">
+      <div className="container">
+        <div className="mainVisual">
+          <div className="logo">
+            <img src="./images/logo.png" alt="logo" />
+          </div>
+          <div class="text">
+            <p>끊임없는 연구와 투자로 인한</p>
+            <p>“우수한 품질과 높은 강도”</p>
+            <p>변색 걱정없는 반영구성</p>
+          </div>
+          <div className="bgImg">
+            <img src="./images/mv_1.jpeg" alt="mv" />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Main;
