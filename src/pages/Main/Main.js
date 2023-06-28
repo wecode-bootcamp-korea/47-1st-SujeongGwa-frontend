de6fbe29ea107a1198e361d56654587e361ec7c7
최진이 <jini.choi@choejin-iui-MacBookAir.local>
2023-06-28 20:33:50 +0900

    ADD : main layout 구현 완료

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -17,27 +17,42 @@ const Main = () => {
             </div>
           </div>
           <div className="right bgImg">
-            <img src="./images/mv_1.jpeg" alt="mv" />
+            <img src="./images/mv_2.jpeg" alt="mv" />
           </div>
         </div>
         <div className="mainContents">
           {MAIN_CONTENTS_LIST.map(info => (
-            <section className={`slide ${info.className}`} key={info.id}>
-              <div className="text">
-                <p className="discriptTitle">{info.discriptTitle}</p>
-                <p className="discriptText">{info.discriptText}</p>
-                <p className="itemStory">{info.itemStory}</p>
-              </div>
-              <div className="productBox">
-                {PRODUCT_LIST_01.map(info => (
-                  <div className="product" key={info.id}>
-                    <img src={info.img} alt={info.name} />
-                    <p>{info.name}</p>
-                    <span>{info.type}</span>
+            <div key={info.id}>
+              <section className={info.SlideClassName}>
+                <div className="text">
+                  <p className="discriptTitle">{info.SlideDiscriptTitle}</p>
+                  <p className="discriptText">{info.SlideDiscriptText}</p>
+                  <p className="itemStory">{info.SlideItemStory}</p>
+                </div>
+                <div className="productBox">
+                  {PRODUCT_LIST_01.map(info => (
+                    <div className="product" key={info.id}>
+                      <img src={info.img} alt={info.name} />
+                      <p>{info.name}</p>
+                      <span>{info.type}</span>
+                    </div>
+                  ))}
+                </div>
+              </section>
+
+              <section className={info.SceneClassName}>
+                <div className="sceneBox">
+                  <div className="scene">
+                    <img src={info.SceneImg} alt="현장이미지" />
                   </div>
-                ))}
-              </div>
-            </section>
+                </div>
+                <div className="text">
+                  <p className="discriptTitle">{info.SceneDiscriptTitle}</p>
+                  <p className="discriptText">{info.SceneDiscriptText}</p>
+                  <p className="itemStory">{info.SceneItemStory}</p>
+                </div>
+              </section>
+            </div>
           ))}
         </div>
       </div>
@@ -47,36 +62,60 @@ const Main = () => {
 
 export default Main;
 
+const MAIN_CONTENTS_LIST = [
+  {
+    id: 1,
+    SlideClassName: 'mainContetnSlide01',
+    SlideDiscriptTitle: '건축용 스톤이라면',
+    SlideDiscriptText:
+      '외부 환경에 강해야 한다. 외벽부터 실내까지 연출이 다양해야한다. 라돈 걱정 없이 안전해야 한다.',
+    SlideItemStory:
+      '자연석에 바라는 모든 것, 더 튼튼하게, 더 아름답게, 더 안전하게',
+    SceneClassName: 'mainCotentScene01',
+    SceneImg: './images/mc_1.jpeg',
+    SceneDiscriptTitle: '건축용 스톤이라면',
+    SceneDiscriptText:
+      '외부 환경에 강해야 한다. 외벽부터 실내까지 연출이 다양해야한다. 라돈 걱정 없이 안전해야 한다.',
+    SceneItemStory:
+      '자연석에 바라는 모든 것, 더 튼튼하게, 더 아름답게, 더 안전하게',
+  },
+  {
+    id: 2,
+    SlideClassName: 'mainContetnSlide02',
+    SlideDiscriptTitle: '건축용 스톤이라면',
+    SlideDiscriptText:
+      '외부 환경에 강해야 한다. 외벽부터 실내까지 연출이 다양해야한다. 라돈 걱정 없이 안전해야 한다.',
+    SlideItemStory:
+      '자연석에 바라는 모든 것, 더 튼튼하게, 더 아름답게, 더 안전하게',
+    SceneClassName: 'mainCotentScene02',
+    SceneImg: './images/mc_2.jpeg',
+    SceneDiscriptTitle: '건축용 스톤이라면',
+    SceneDiscriptText:
+      '외부 환경에 강해야 한다. 외벽부터 실내까지 연출이 다양해야한다. 라돈 걱정 없이 안전해야 한다.',
+    SceneItemStory:
+      '자연석에 바라는 모든 것, 더 튼튼하게, 더 아름답게, 더 안전하게',
+  },
+  {
+    id: 3,
+    SlideClassName: 'mainContetnSlide03',
+    SlideDiscriptTitle: '건축용 스톤이라면',
+    SlideDiscriptText:
+      '외부 환경에 강해야 한다. 외벽부터 실내까지 연출이 다양해야한다. 라돈 걱정 없이 안전해야 한다.',
+    SlideItemStory:
+      '자연석에 바라는 모든 것, 더 튼튼하게, 더 아름답게, 더 안전하게',
+    SceneClassName: 'mainCotentScene03',
+    SceneImg: './images/mc_3.jpeg',
+    SceneDiscriptTitle: '건축용 스톤이라면',
+    SceneDiscriptText:
+      '외부 환경에 강해야 한다. 외벽부터 실내까지 연출이 다양해야한다. 라돈 걱정 없이 안전해야 한다.',
+    SceneItemStory:
+      '자연석에 바라는 모든 것, 더 튼튼하게, 더 아름답게, 더 안전하게',
+  },
+];
+
 const PRODUCT_LIST_01 = [
   { id: 1, img: './images/p_1_1.jpeg', name: 'SPM 1103', type: 'MATT' },
   { id: 2, img: './images/p_1_2.jpeg', name: 'SPM 3055', type: 'MATT' },
   { id: 3, img: './images/p_1_3.jpeg', name: 'SPM 3056', type: 'MATT' },
   { id: 4, img: './images/p_1_1.jpeg', name: 'SPM 1103', type: 'MATT' },
-];
-
-const MAIN_CONTENTS_LIST = [
-  {
-    id: 1,
-    className: 'mc01',
-    discriptTitle: '건축용 스톤이라면',
-    discriptText:
-      '외부 환경에 강해야 한다. 외벽부터 실내까지 연출이 다양해야한다. 라돈 걱정 없이 안전해야 한다.',
-    itemStory: '자연석에 바라는 모든 것, 더 튼튼하게, 더 아름답게, 더 안전하게',
-  },
-  {
-    id: 2,
-    className: 'mc02',
-    discriptTitle: '건축용 스톤이라면',
-    discriptText:
-      '외부 환경에 강해야 한다. 외벽부터 실내까지 연출이 다양해야한다. 라돈 걱정 없이 안전해야 한다.',
-    itemStory: '자연석에 바라는 모든 것, 더 튼튼하게, 더 아름답게, 더 안전하게',
-  },
-  {
-    id: 3,
-    className: 'mc03',
-    discriptTitle: '건축용 스톤이라면',
-    discriptText:
-      '외부 환경에 강해야 한다. 외벽부터 실내까지 연출이 다양해야한다. 라돈 걱정 없이 안전해야 한다.',
-    itemStory: '자연석에 바라는 모든 것, 더 튼튼하게, 더 아름답게, 더 안전하게',
-  },
 ];
