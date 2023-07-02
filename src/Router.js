1a5f44b2e992221042557880b5c407db89d4f83f
Kimsu <betnwjd25@gmail.com>
2023-07-02 20:13:41 +0900

    ADD: 404page layout임시

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import Nav from './components/Nav/Nav';
 import SignUp from './pages/SignUp/SignUp';
 import Login from './pages/Login/Login';
 import Footer from './components/Footer/Footer';
+import NotFound from './components/NotFound/NotFound';
 
 const Router = () => {
   return (
@@ -14,6 +15,7 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="/users/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>
