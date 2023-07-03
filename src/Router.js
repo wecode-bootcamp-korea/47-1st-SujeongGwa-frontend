0bb43d4233737a99ecdff070b60f64ebd7279179
Kimsu <betnwjd25@gmail.com>
2023-07-03 12:19:46 +0900

    mypage 레이아웃 구현

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import Nav from './components/Nav/Nav';
 import SignUp from './pages/SignUp/SignUp';
 import Login from './pages/Login/Login';
 import Footer from './components/Footer/Footer';
+import MyPage from './pages/MyPage/MyPage';
 
 const Router = () => {
   return (
@@ -14,6 +15,7 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="/users/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
