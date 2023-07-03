ab596703ce903bf58b90749690d90f7bdaac84be
최진이 <jini.choi@choejin-iui-MacBookAir.local>
2023-07-03 23:01:32 +0900

    ADD : main 기능 구현 완료

---
+++ src/pages/Main/mainContentsData.js
@@ -0,0 +1,184 @@
+export const MAIN_CONTENTS_LIST = [
+  {
+    id: 1,
+    SlideClassName: 'mainContetnSlide01',
+    SlideDiscriptTitle: '건축용 스톤이라면',
+    SlideDiscriptText:
+      '외부 환경에 강해야 한다. 외벽부터 실내까지 연출이 다양해야한다. 라돈 걱정 없이 안전해야 한다.',
+    SlideItemStory:
+      '자연석에 바라는 모든 것, 더 튼튼하게, 더 아름답게, 더 안전하게',
+    SlideProductList: [
+      {
+        id: 1,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/P_1_CTN_01_1.jpg',
+        name: ' P_1_CTN_01_1',
+        type: 'MATT',
+      },
+      {
+        id: 2,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/P_1_CTN_02_1.jpg',
+        name: 'P_1_CTN_02_1',
+        type: 'MATT',
+      },
+      {
+        id: 3,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/P_1_CTN_03_1.jpg',
+        name: 'P_1_CTN_03_1',
+        type: 'MATT',
+      },
+      {
+        id: 4,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/P_2_SDH_01_1.jpg',
+        name: 'P_2_SDH_01_1',
+        type: 'MATT',
+      },
+      {
+        id: 5,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/P_2_SDH_02_1.jpg',
+        name: 'P_2_SDH_02_1',
+        type: 'MATT',
+      },
+      {
+        id: 6,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/P_2_SDH_03_1.jpg',
+        name: 'P_2_SDH_03_1',
+        type: 'MATT',
+      },
+    ],
+    SceneClassName: 'mainCotentScene01',
+    SceneImg:
+      'https://raw.githubusercontent.com/Teachsue/Image_URL/main/porcelain_tile/porcelain_tile%201.jpg',
+    SceneDiscriptTitle: '건축용 스톤이라면',
+    SceneDiscriptText: '반복되는 형식에 의해 강화된 돌의 Real 아름다움,',
+    SceneItemStory: '작은 공간부터 큰 공간까지 가득채우는 친환경적인 돌입니다.',
+  },
+  {
+    id: 2,
+    SlideClassName: 'mainContetnSlide02',
+    SlideDiscriptTitle: '건축용 스톤이라면',
+    SlideDiscriptText:
+      '포근한 공간의 햇빛처럼 섬세하고 따스함을 위해 나온 디자인.',
+    SlideItemStory:
+      '밝은 톤의 타일들로 구성된 타일들이 여러분의 공간에 따스함을 안겨 줍니다.',
+    SlideProductList: [
+      {
+        id: 1,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/F_2_KSW_01_3.jpg',
+        name: ' F_2_KSW_01_3',
+        type: 'MATT',
+      },
+      {
+        id: 2,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/F_2_KSW_02_3.jpg',
+        name: 'F_2_KSW_02_3',
+        type: 'MATT',
+      },
+      {
+        id: 3,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/F_2_KSW_03_3.jpg',
+        name: 'F_2_KSW_03_3',
+        type: 'MATT',
+      },
+      {
+        id: 4,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/F_2_KSW_04_3.jpg',
+        name: 'F_2_KSW_04_3',
+        type: 'SOFT MATT',
+      },
+      {
+        id: 5,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/F_2_KSW_04_3.jpg',
+        name: 'F_2_KSW_05_3',
+        type: 'SOFT MATT',
+      },
+      {
+        id: 6,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/F_2_KSW_04_3.jpg',
+        name: 'F_2_KSW_06_3',
+        type: 'SOFT MATT',
+      },
+      {
+        id: 7,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/F_2_KSW_04_3.jpg',
+        name: 'F_2_KSW_06_3',
+        type: 'SOFT MATT',
+      },
+      {
+        id: 8,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/F_2_KSW_04_3.jpg',
+        name: 'F_2_KSW_06_3',
+        type: 'SOFT MATT',
+      },
+    ],
+    SceneClassName: 'mainCotentScene02',
+    SceneImg:
+      'https://raw.githubusercontent.com/Teachsue/Image_URL/main/wall_tile/wall_tile%201.jpg',
+    SceneDiscriptTitle: '건축용 스톤이라면',
+    SceneDiscriptText: '새로운 사이즈, 새로운 아이템 ',
+    SceneItemStory: '그리고 새로운 스타일...',
+  },
+  {
+    id: 3,
+    SlideClassName: 'mainContetnSlide03',
+    SlideDiscriptTitle: '건축용 스톤이라면',
+    SlideDiscriptText: '얇고 강하게 또 가볍게',
+    SlideItemStory:
+      '충격 습기, 오염에 약한 일반타일보다 강하게 개발되어 강한 내구성을 지닌 타일.',
+    SlideProductList: [
+      {
+        id: 1,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/W_1_SDH_01_1.jpg',
+        name: ' P_1_CTN_01_1',
+        type: 'MATT',
+      },
+      {
+        id: 2,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/W_1_SDH_02_1.jpg',
+        name: 'P_1_CTN_02_1',
+        type: 'GLOSSY',
+      },
+      {
+        id: 3,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/W_1_SDH_03_1.jpg',
+        name: 'P_1_CTN_03_1',
+        type: 'GLOSSY',
+      },
+      {
+        id: 4,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/W_1_SDH_04_1.jpg',
+        name: 'P_2_SDH_01_1',
+        type: 'GLOSSY',
+      },
+      {
+        id: 5,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/W_1_SDH_05_1.jpg',
+        name: 'P_2_SDH_02_1',
+        type: 'GLOSSY',
+      },
+      {
+        id: 6,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/W_1_SDH_06_1.jpg',
+        name: 'P_2_SDH_03_1',
+        type: 'MATT',
+      },
+      {
+        id: 7,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/W_1_SDH_05_1.jpg',
+        name: 'P_2_SDH_02_1',
+        type: 'GLOSSY',
+      },
+      {
+        id: 8,
+        img: 'https://raw.githubusercontent.com/Teachsue/Image_URL/main/W_1_SDH_06_1.jpg',
+        name: 'P_2_SDH_03_1',
+        type: 'MATT',
+      },
+    ],
+    SceneClassName: 'mainCotentScene03',
+    SceneImg:
+      'https://raw.githubusercontent.com/Teachsue/Image_URL/main/floor_tile/Floor_tile%201.jpg',
+    SceneDiscriptTitle: '건축용 스톤이라면',
+    SceneDiscriptText: '빠르고 편리한 개량압착 시공',
+    SceneItemStory: '기존 마감재 철거와 하지철물 없이 바로 시공 가능',
+  },
+];
