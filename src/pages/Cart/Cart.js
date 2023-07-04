2490b4523a3d1337c51b5b806d6a683d995f8a4f
최진이 <jini.choi@choejin-iui-MacBookAir.local>
2023-07-05 04:03:42 +0900

    ADD : cart 기능 구현 중

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Count from '../../components/Count/Count';
+import { Link, useNavigate } from 'react-router-dom';
+// import Count from '../../components/Count/Count';
 import './Cart.scss';
 
 const Cart = () => {
@@ -10,34 +10,108 @@ const Cart = () => {
   const [prices, setTotalPrice] = useState(0);
 
   useEffect(() => {
-    fetch('./data/cartListData.json')
+    fetch('./data/cartListData.json', {
+      method: 'GET',
+      // headers: {
+      //   authorization:
+      //     'Bearer eyNzYwMzIsImV4cCI6MTY4OTI1MzYzMn0.1ntICFfbpT3HlDUODlV-sI-Fo2i_JxNruJwmlUtbF6w',
+      // },JhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjM3LCJpYXQiOjE2ODg0
+    })
       .then(res => res.json())
       .then(data => {
-        setItems(data.data);
         setOriginalItems(data.data);
-        setTotalWeight(calculateTotalWeight(data.data));
-        setTotalPrice(calculateTotalPrice(data.data));
+        const updatedData = data.data.map(item => ({
+          ...item,
+          count: item.quantity,
+          weight: item.weight * item.quantity,
+          price: item.price * item.quantity,
+          // 프로퍼티를 복사한후 count프로퍼티를 추가하고 초기값으로 item.quantity를 가지게 됨
+        }));
+        setItems(updatedData);
+        setTotalWeight(calculateTotalWeight(updatedData));
+        setTotalPrice(calculateTotalPrice(updatedData));
       });
   }, []);
 
-  const setCountArray = (cartId, count) => {
-    setItems(prevItems => {
-      const updatedItems = prevItems.map(data => {
-        if (data.cartId === cartId) {
-          const originalItem = originalItems.find(i => i.cartId === cartId);
-          const updatedItem = { ...data, count: count };
-          updatedItem.weight = originalItem.weight * count;
-          updatedItem.price = parseFloat(originalItem.price) * count;
-          return updatedItem;
-        }
-        return data;
-      });
+  // const makePayment = () => {
+  //   // Create an array to store the cart items data
+  //   const cartItems = [];
 
-      setTotalWeight(calculateTotalWeight(updatedItems));
-      setTotalPrice(calculateTotalPrice(updatedItems));
+  //   console.log(cartItems);
+  //   // Loop through the items to get the cartId and countNumber
+  //   items.forEach(item => {
+  //     const { cartId, count } = item;
+  //     cartItems.push({
+  //       productId: cartId,
+  //       quantity: count,
+  //     });
+  //   });
 
-      return updatedItems;
-    });
+  //   fetch('http://10.58.52.235:3000/order', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json;charset=utf-8',
+  //     },
+  //     body: JSON.stringify(cartItems),
+  //   })
+  //     .then(res => {
+  //       if (res.ok === true) {
+  //         return res.json();
+  //       }
+  //     })
+  //     .then(data => {
+  //       if (data && data.message === 'SUCCESS_CREATE_CART') {
+  //         navigate('/order');
+  //       }
+  //     });
+  // };
+
+  const decrease = (items, cartId) => {
+    return items.map(item =>
+      item.cartId === cartId && item.count > 1
+        ? {
+            ...item,
+            count: item.count - 1,
+            weight:
+              originalItems.find(i => i.cartId === cartId).weight *
+              (item.count - 1),
+            price:
+              originalItems.find(i => i.cartId === cartId).price *
+              (item.count - 1),
+          }
+        : item
+    );
+  };
+
+  const increase = (items, cartId) => {
+    return items.map(item =>
+      item.cartId === cartId
+        ? {
+            ...item,
+            count: item.count + 1,
+            weight:
+              originalItems.find(i => i.cartId === cartId).weight *
+              (item.count + 1),
+            price:
+              originalItems.find(i => i.cartId === cartId).price *
+              (item.count + 1),
+          }
+        : item
+    );
+  };
+
+  const handleDecrease = cartId => {
+    const newItems = decrease(items, cartId);
+    setItems(newItems);
+    setTotalWeight(calculateTotalWeight(newItems));
+    setTotalPrice(calculateTotalPrice(newItems));
+  };
+
+  const handleIncrease = cartId => {
+    const newItems = increase(items, cartId);
+    setItems(newItems);
+    setTotalWeight(calculateTotalWeight(newItems));
+    setTotalPrice(calculateTotalPrice(newItems));
   };
 
   const calculateTotalWeight = items => {
@@ -51,8 +125,33 @@ const Cart = () => {
   const showAlert = weights > 1000;
 
   const onRemove = cartId => {
-    const itemsFilter = items.filter(data => data.cartId !== cartId);
-    setItems(itemsFilter);
+    return items.filter(data => data.cartId !== cartId);
+  };
+
+  const handleRemove = cartId => {
+    const newItems = onRemove(cartId);
+    setItems(newItems);
+    setTotalWeight(calculateTotalWeight(newItems));
+    setTotalPrice(calculateTotalPrice(newItems));
+  };
+
+  const sub_categories = [
+    '600x600x10mm',
+    '600x600x20mm',
+    '600x1200x11mm',
+    '600x1200x20mm',
+    '400x800x11mm',
+    '300x600x9mm',
+    '200x600x9mm',
+    '300x300x9mm',
+    '200x400x9mm',
+  ];
+
+  const findSize = subCategoryId => {
+    const foundSizes = sub_categories.filter(
+      (type, idx) => idx + 1 === subCategoryId
+    );
+    return foundSizes.length > 0 ? foundSizes[0] : '';
   };
 
   return (
@@ -65,30 +164,43 @@ const Cart = () => {
               <li key={info.id}>{info.name}</li>
             ))}
           </ul>
-          <div className="cartEmpty">
-            <p>장바구니가 비었습니다.</p>
-          </div>
-          {/* <div className="cartListBox">
-            {items.map(el => (
-              <ul className="cartList" key={el.cartId}>
-                <li>{el.kindId}</li>
-                <li>{el.sizeId}</li>
-                <li>{el.tileName}</li>
-                <li>{Number(el.weight)}</li>
-                <li>
-                  <Count
-                    count={el.count}
-                    setCount={newCount => setCountArray(el.cartId, newCount)}
-                    isDisabled={showAlert}
-                  />
-                </li>
-                <li>{Number(el.price)} 원</li>
-                <li className="deletBtn">
-                  <button onClick={() => onRemove(el.cartId)}>X</button>
-                </li>
-              </ul>
-            ))}
-          </div> */}
+          {items.length === 0 && (
+            <div className="cartEmpty">
+              <p>장바구니가 비었습니다.</p>
+            </div>
+          )}
+          {items.length !== 0 && (
+            <div className="cartListBox">
+              {items.map(el => (
+                <ul className="cartList" key={el.cartId}>
+                  <li>{el.kindId}</li>
+                  <li>{findSize(el.surfaceTypeId)}</li>
+                  <li>{el.tileName}</li>
+                  <li>{Number(el.weight)}</li>
+                  <li>
+                    <div className="count">
+                      <div className="countInput">
+                        <button onClick={() => handleDecrease(el.cartId)}>
+                          -
+                        </button>
+                        <div className="countInputText">{el.count}</div>
+                        <button
+                          onClick={() => handleIncrease(el.cartId)}
+                          disabled={showAlert || items.length === 0}
+                        >
+                          +
+                        </button>
+                      </div>
+                    </div>
+                  </li>
+                  <li>{Number(el.price)} 원</li>
+                  <li className="deletBtn">
+                    <button onClick={() => handleRemove(el.cartId)}>X</button>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          )}
         </div>
         <div className="right">
           <div className="totalBox">
@@ -125,8 +237,16 @@ const Cart = () => {
             </li>
             <li>
               <Link to="/order">
-                <button className="blackBtn" disabled={showAlert}>
-                  결제하기
+                {/* <button
+                  className="blackBtn"
+                  disabled={showAlert || items.length === 0}
+                  onClick={makePayment}
+                > */}
+                <button
+                  className="blackBtn"
+                  disabled={showAlert || items.length === 0}
+                >
+                  주문하기
                 </button>
               </Link>
             </li>
