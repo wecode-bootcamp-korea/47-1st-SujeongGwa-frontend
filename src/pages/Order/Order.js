f0e86ad07050b818e2bcc849a57c8b3da78405e9
Kimsu <betnwjd25@gmail.com>
2023-07-05 15:13:50 +0900

    ADD: 레이아웃 및 변경된 기능 구현

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -1,67 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import './Order.scss';
-import Count from '../../components/Count/Count';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Order = () => {
   const [items, setItems] = useState([]);
-  const [originalItems, setOriginalItems] = useState([]);
-  const [users, setUsers] = useState([]);
-  const [weights, setTotalWeight] = useState(0);
-  const [prices, setTotalPrice] = useState(0);
   const navigate = useNavigate();
 
-  //카트에 GET, PATCH
+  const surface_type = ['Matt', 'Hard Matt', 'Soft Matt', 'LappaTo', 'Glossy'];
+  const sub_categories = [
+    { id: 1, sizes: '600x600x10mm' },
+    { id: 2, sizes: '600x600x20mm' },
+    { id: 3, sizes: '600x1200x11mm' },
+    { id: 4, sizes: '600x1200x20mm' },
+    { id: 5, sizes: '400x800x11mm' },
+    { id: 6, sizes: '300x600x9mm' },
+    { id: 7, sizes: '200x600x9mm' },
+    { id: 8, sizes: '300x300x9mm' },
+    { id: 9, sizes: '200x400x9mm' },
+  ];
+
+  const findSize = subCategoryId => {
+    return sub_categories.find(el => el.id === subCategoryId).sizes;
+  };
+
+  const findType = surfaceType => {
+    const foundType = surface_type.filter(
+      (type, idx) => idx + 1 === surfaceType
+    );
+    return foundType.length > 0 ? foundType[0] : '';
+  };
+
+  //카트에서 GET
   useEffect(() => {
     fetch('./data/order.json')
       .then(res => res.json())
       .then(data => {
         setItems(data.data);
-        setOriginalItems(data.data);
-        setTotalWeight(calculateTotalWeight(data.data));
-        setTotalPrice(calculateTotalPrice(data.data));
       });
   }, []);
 
-  useEffect(() => {
-    fetch('./data/user.json')
-      .then(res => res.json())
-      .then(data => setUsers(data.user));
-  }, []);
-
-  useEffect(() => {
-    if (items.length === 0) {
-      setTotalWeight(0);
-      setTotalPrice(0);
-    }
-  }, [items]);
-
-  const setCountArray = (itemId, count) => {
-    setItems(prevItems => {
-      const updatedItems = prevItems.map(item => {
-        if (item.id === itemId) {
-          const originalItem = originalItems.find(i => i.id === itemId);
-          const updatedItem = { ...item, count: count };
-          updatedItem.weight = originalItem.weight * count;
-          updatedItem.price = Number(originalItem.price) * count;
-          return updatedItem;
-        }
-        return item;
-      });
-
-      setTotalWeight(calculateTotalWeight(updatedItems));
-      setTotalPrice(calculateTotalPrice(updatedItems));
-
-      return updatedItems;
-    });
+  const totalWeight = items => {
+    return items?.reduce((acc, cur) => acc + Number(cur.weight), 0);
   };
 
-  const calculateTotalWeight = items => {
-    return items.reduce((total, item) => total + Number(item.weight), 0);
-  };
-
-  const calculateTotalPrice = items => {
-    return items.reduce((total, item) => total + Number(item.price), 0);
+  const totalPrice = items => {
+    return items?.reduce((acc, cur) => acc + Number(cur.price), 0);
   };
 
   const [inputValues, setInputValues] = useState({});
@@ -72,49 +55,38 @@ const Order = () => {
       [name]: value,
     }));
   };
-  const totalWeight = weights;
-  const showAlert = totalWeight > 1000;
 
-  const onRemove = itemId => {
-    setItems(prevItems => {
-      const itemsFilter = prevItems.filter(data => data.id !== itemId);
-      setTotalWeight(calculateTotalWeight(itemsFilter));
-      setTotalPrice(calculateTotalPrice(itemsFilter));
-      return itemsFilter;
-    });
-  };
-
-  //order에 POST
-  const postProduct = () => {
-    fetch('api/oder/주문번호', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-      body: JSON.stringify({
-        address: inputValues.address,
-        total_price: prices,
-        total_weight: weights,
-      }),
-    })
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        if (data.message === 'CREATE_USER_SUCCESS!') {
-          navigate('/orderResult');
-        } else if (data.message === '백엔드 메세지') {
-          alert('구매에 실패하였습니다.');
-        }
-      });
-  };
+  // //order에 POST
+  // const postProduct = () => {
+  //   fetch('api/oder/주문번호', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json;charset=utf-8',
+  //     },
+  //     body: JSON.stringify({
+  //       address: inputValues.address,
+  //       total_price: totalPrice(items),
+  //       total_weight: totalWeight(items),
+  //     }),
+  //   })
+  //     .then(res => {
+  //       return res.json();
+  //     })
+  //     .then(data => {
+  //       if (data.message === 'CREATE_USER_SUCCESS!') {
+  //         navigate('/orderResult');
+  //       } else if (data.message === '백엔드 메세지') {
+  //         alert('구매에 실패하였습니다.');
+  //       }
+  //     });
+  // };
 
   return (
     <div className="order">
-      {users.map(el => (
-        <div className="buyerInfo" key={el.id}>
+      <div className="bigBox">
+        <div className="buyerInfo">
           <h1>주문자 정보</h1>
-          <p className="userInfo"> {el.name} </p>
+          <p className="userInfo"> {items[0]?.name} 님</p>
           <div className="shippingAddress">
             <h2>주문을 어디로 보내시겠습니까?</h2>
             <div className="orderTypeName"> 이름</div>
@@ -140,90 +112,56 @@ const Order = () => {
             />
           </div>
         </div>
-      ))}
-      <div className="perchaseBox">
-        <div className="payBox">
-          <div className="indexBox">
-            <p className="resultText"> 최종가격</p>
-            <p className="resultText"> 총 무게</p>
-          </div>
-          <div className="final">
-            <p className="resultText">{prices} 원</p>
-            <p className="resultText">{weights} KG</p>
-          </div>
-        </div>
-        {items.length === 0 && (
-          <div className="orderEmpty">
-            <p>구매할 물품이 없습니다.</p>
-          </div>
-        )}
-        {items.map(el => (
-          <div className="total" key={el.id}>
-            <div className="item">
-              <img
-                src={el.image_url}
-                alt="perchaseProduct"
-                className="perchaseImage"
-              />
-              <div className="perchaseOption">
-                <p className="descriptionOption">{el.name}</p>
-                <p className="descriptionOption">{el.surface_type_id}</p>
-                <p className="descriptionOption">{el.sub_category_id}</p>
-              </div>
-              <div className="perchaseOption">
-                <Count
-                  className="count"
-                  count={el.sell_counts}
-                  setCount={newCount => setCountArray(el.id, newCount)}
-                  isDisabled={showAlert}
-                />
-                <p className="descriptionOption">{el.weight} KG</p>
-                <p className="descriptionOption">{Number(el.price)} 원</p>
-              </div>
-              <div className="deleteBox">
-                <button
-                  type="submit"
-                  className="deleteButton"
-                  onClick={() => onRemove(el.id)}
-                >
-                  ✕
-                </button>
-              </div>
+        <div className="perchaseBox">
+          <div className="payBox">
+            <div className="indexBox">
+              <p className="resultText"> 최종가격</p>
+              <p className="resultText"> 총 무게</p>
+            </div>
+            <div className="final">
+              <p className="resultText">{totalPrice(items)} 원</p>
+              <p className="resultText">{totalWeight(items)} KG</p>
             </div>
           </div>
-        ))}
-        <div className="buttonBox">
-          <button
-            type="submit"
-            className="payment"
-            onClick={() => {
-              navigate('/orderResult');
-            }}
-            disabled={showAlert}
-          >
-            결제하기
-          </button>
-          {items.length === 0 && (
+          <div className="total">
+            {items.map(el => (
+              <div className="item" key={el.cartId}>
+                <img
+                  src={el.imageUrl}
+                  alt="perchaseProduct"
+                  className="perchaseImage"
+                />
+                <div className="perchaseOption">
+                  <p className="descriptionOption">{el.tileName}</p>
+                  <p className="descriptionOption">
+                    {findSize(el.surfaceTypeId)}
+                  </p>
+                  <p className="descriptionOption">
+                    {findType(el.surfaceTypeId)}
+                  </p>
+                </div>
+                <div className="perchaseOption">
+                  <p className="descriptionOption">{el.quantity} 개</p>
+                  <p className="descriptionOption">{el.weight} KG</p>
+                  <p className="descriptionOption">{Number(el.price)} 원</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="buttonBox">
             <button
-              className="gotoHome"
+              type="submit"
+              className="payment"
               onClick={() => {
-                navigate('/');
+                navigate('/orderResult');
               }}
             >
-              홈으로
+              결제하기
             </button>
-          )}
-          {showAlert && (
-            <div className="alertTextBox">
-              <p className="alertText">
-                무게는 1000Kg 이상 구매하실 수 없습니다.
-              </p>
-            </div>
-          )}
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default Order;
