7c226f88f1bdf2c723f2626989fe61ef10210d3b
Kimsu <betnwjd25@gmail.com>
2023-07-06 10:17:02 +0900

    저장용

--- src/pages/OrderResult/OrderResult.js
+++ src/pages/OrderResult/OrderResult.js
@@ -1,94 +1,70 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import './OrderResult.scss';
 
 const OrderResult = () => {
   const navigate = useNavigate();
-  const [result, setResult] = useState(null);
+  const location = useLocation();
+  const info = location.state;
 
-  useEffect(() => {
-    fetch('http://api주소:3000/oder', {
-      method: 'GET',
-      headers: {
-        authorization: 'Bearer 어쩌고저쩌고',
-      },
-    })
-      .then(res => res.json())
-      .then(data => {
-        setResult([data.data]);
-        // console.log(data);
-        // console.log('가져온 데이터:', data);
-      });
-  }, []);
-
+  const handleGoMain = () => {
+    navigate('/');
+  };
   return (
     <div className="orderResult">
-      {result &&
-        result.map(data => (
-          <div key={data.order_number}>
-            <div className="orderText">
-              <h1>감사합니다, {data.name} 님</h1>
-              <p className="thanksText">
-                주문이 완료되었습니다. <br />
-                추가 문의 사항이 있으시면 연락 주시기 바랍니다.
-              </p>
-            </div>
-            <div className="orderNumberBox">
-              <li>주문번호</li>
-              <li>{data.order_number}</li>
-            </div>
-
-            <div className="orderInfoBox">
-              <ul className="orderItemBox">
-                {ORDER_CATEGORY.map(el => (
-                  <li key={el.id}>{el.name}</li>
-                ))}
-              </ul>
-              <div className="orderItemBox">
-                <div className="orderItemBox2">
-                  {data.product.map((productData, index) => (
-                    <ul className="column" key={index}>
-                      <li>{productData.name}</li>
-                      <li>{productData.surface_type_id}</li>
-                      <li>{productData.quantity} 개</li>
-                    </ul>
-                  ))}
-                </div>
-              </div>
-
-              <div className="orderNumberBox2">
-                <li> 총 무게 : {data.total_weight} Kg</li>
-                <li> 총 가격 : {data.total_price} 원</li>
-              </div>
-            </div>
-            <div className="orderUserInfoBox">
-              <div className="orderUserBox">
-                {ORDERUSER_CATEGORY.map(el => (
-                  <ul className="orderUserInfo" key={el.id}>
-                    <li>{el.name}</li>
-                  </ul>
-                ))}
-              </div>
-              <div className="orderUserBox">
-                <ul className="orderUserInfo">
-                  <li>{data.name}</li>
-                  <li>{data.email}</li>
-                  <li>POINT(포인트)</li>
-                  <li>{data.name}</li>
-                  <li>{data.address}</li>
-                  <li>경비실에 맡겨주세요</li>
-                </ul>
-              </div>
-            </div>
+      <div className="orderText">
+        <h1>감사합니다, {info.userName} 님</h1>
+        <p className="thanksText">
+          주문이 완료되었습니다. <br />
+          추가 문의 사항이 있으시면 연락 주시기 바랍니다.
+        </p>
+      </div>
+      <div className="orderNumberBox">
+        <li>주문번호</li>
+        <li>{info.orderNumber}</li>
+      </div>
+      <div className="orderInfoBox">
+        <ul className="orderItemBox">
+          {ORDER_CATEGORY.map(el => (
+            <li key={el.id}>{el.name}</li>
+          ))}
+        </ul>
+        <div className="orderItemBox">
+          <div className="orderItemBox2">
+            <ul className="column">
+              <li>{info.product.name}</li>
+              <li>{info.product.type}</li>
+              <li>{info.product.quantity} 개</li>
+            </ul>
           </div>
-        ))}
+        </div>
+        <div className="orderNumberBox2">
+          <li> 총 무게 : {info.weight} Kg</li>
+          <li> 총 가격 : {info.price} 원</li>
+        </div>
+      </div>
+      <div className="orderUserInfoBox">
+        <div className="orderUserBox">
+          {ORDERUSER_CATEGORY.map(el => (
+            <ul className="orderUserInfo" key={el.id}>
+              <li>{el.name}</li>
+            </ul>
+          ))}
+
+          <div className="orderUserBox">
+            <ul className="orderUserInfo">
+              <li>{info.userName}</li>
+              <li>{email}</li>
+              <li>POINT(포인트)</li>
+              <li>{name}</li>
+              <li>{address}</li>
+              <li>경비실에 맡겨주세요</li>
+            </ul>
+          </div>
+        </div>
+      </div>
       <div className="gotoHomeBox">
-        <button
-          className="gotoHome"
-          onClick={() => {
-            navigate('/');
-          }}
-        >
+        <button className="gotoHome" onClick={handleGoMain}>
           메인페이지로 이동
         </button>
       </div>
